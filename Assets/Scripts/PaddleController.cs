87460a27cb3d1a15cf2fe9b71ee2fba8cda0462f
helmy-faishal <helmy6941@gmail.com>
2023-09-12 19:22:32 +0700

    Menambahkan PaddleController dan Physics

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public KeyCode keyPress = KeyCode.Z;
+    public HingeJoint hinge;
+    public float targetPressed;
+    public float targetReleased;
+
+    private void Awake()
+    {
+        hinge = GetComponent<HingeJoint>();
+        targetPressed = hinge.limits.max;
+        targetReleased = hinge.limits.min;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePaddle();
+    }
+
+    void MovePaddle()
+    {
+        JointSpring jointSpring = hinge.spring;
+
+        jointSpring.targetPosition = targetReleased;
+        if (Input.GetKey(keyPress))
+        {
+            jointSpring.targetPosition = targetPressed;
+        }
+
+        hinge.spring = jointSpring;
+    }
+}
