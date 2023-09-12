a3b686820763cabda3d452f1dd6d8520e3e9a3c3
helmy-faishal <helmy6941@gmail.com>
2023-09-12 23:37:24 +0700

    Menambahkan animasi saat hit Bumper

--- Assets/Scripts/BumperController.cs
+++ Assets/Scripts/BumperController.cs
@@ -7,10 +7,12 @@ public class BumperController : MonoBehaviour
     public float multiplier = 2f;
     public Color color;
     private Renderer render;
+    private Animator animator;
 
     private void Awake()
     {
         render = GetComponent<Renderer>();
+        animator = GetComponent<Animator>();
         render.material.color = color;
     }
     private void OnCollisionEnter(Collision collision)
@@ -19,6 +21,7 @@ public class BumperController : MonoBehaviour
         {
             Rigidbody ball = collision.collider.gameObject.GetComponent<Rigidbody>();
             ball.velocity *= multiplier;
+            animator.SetTrigger("hit");
         }
     }
 }
